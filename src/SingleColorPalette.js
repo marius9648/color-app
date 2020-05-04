2e54e4ae51cd6aae1865818ef5a0d2e5c90f72b2
Marius <marius9648@yahoo.com>
2020-05-04 15:10:00 +0300

    Move out styles

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -3,49 +3,8 @@ import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
 import { withStyles } from '@material-ui/styles';
+import styles from './styles/PaletteStyles';
 import ColorBox from './ColorBox';
-
-const styles = {
-  Pdalette: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-
-  colors: {
-    height: '90%',
-  },
-
-  goBack: {
-    width: '20%',
-    height: '50%',
-    margin: '0 auto',
-    display: 'inline-block',
-    position: 'relative',
-    cursor: 'pointer',
-    marginBottom: '-3.5px',
-    opacity: 1,
-    backgroundColor: 'black',
-    '& a': {
-      color: 'white',
-      width: '100px',
-      height: '30px',
-      position: 'absolute',
-      display: 'inline-block',
-      top: '50%',
-      left: '50%',
-      marginLeft: '-50px',
-      marginTop: '-15px',
-      textAlign: 'center',
-      background: 'rgba(255, 255, 255, 0.3)',
-      fontSize: '1rem',
-      lineHeight: '30px',
-      textTransform: 'uppercase',
-      border: 'none',
-      textDecoration: 'none',
-    },
-  },
-};
 
 class SingleColorPalette extends Component {
   constructor(props) {
@@ -88,7 +47,7 @@ class SingleColorPalette extends Component {
       />
     ));
     return (
-      <div className={classes.Pdalette}>
+      <div className={classes.Palette}>
         <Navbar handleChange={this.changeFormat} showingAllColors={false} />
         <div className={classes.colors}>
           {colorBoxes}
