ac83229a28d109d85c72902d3dc8f963d7d380ee
Marius <marius9648@yahoo.com>
2020-05-11 18:50:03 +0300

    Finish MiniPalette delete

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,27 +1,30 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import MiniPalette from "./MiniPalette";
-import { withStyles } from "@material-ui/styles";
-import styles from "./styles/PaletteListStyles";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/PaletteListStyles';
 
 class PaletteList extends Component {
   goToPalette(id) {
     this.props.history.push(`/palette/${id}`);
   }
   render() {
-    const { palettes, classes } = this.props;
+    const { palettes, classes, deletePalette } = this.props;
     return (
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors</h1>
-            <Link to='/palette/new'>Create Palette</Link>
+            <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
-            {palettes.map(palette => (
+            {palettes.map((palette) => (
               <MiniPalette
                 {...palette}
                 handleClick={() => this.goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
               />
             ))}
           </div>
