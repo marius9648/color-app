f63481e375306235d34a932bddaffb945ca549a5
Marius <marius9648@yahoo.com>
2020-05-12 20:56:57 +0300

    Make color box responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,12 +15,12 @@ export default {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: (props) => (props.showingFullPalette ? '25%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '20%' : '33.3333%'),
     },
 
     [sizes.down('md')]: {
       width: '50%',
-      height: (props) => (props.showingFullPalette ? '10%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '10%' : '20%'),
     },
 
     [sizes.down('xs')]: {
@@ -119,6 +119,9 @@ export default {
       marginBottom: '0',
       padding: '1rem',
       textTransform: 'uppercase',
+      [sizes.down('xs')]: {
+        fontSize: '6rem',
+      },
     },
     '& p': {
       fontSize: '2rem',
