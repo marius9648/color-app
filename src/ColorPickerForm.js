c972dfab30b8f4d36f53d8b6aca8cdd229e1aa0b
Marius <marius9648@yahoo.com>
2020-05-14 22:08:41 +0300

    Fix issues with NewPaletteForm

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import { ChromePicker } from "react-color";
-import { withStyles } from "@material-ui/core/styles";
-import styles from "./styles/ColorPickerFormStyles";
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { ChromePicker } from 'react-color';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles/ColorPickerFormStyles';
 
 class ColorPickerForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentColor: "teal", newColorName: "" };
+    this.state = { currentColor: 'teal', newColorName: '' };
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
-    ValidatorForm.addValidationRule("isColorNameUnique", value =>
+    ValidatorForm.addValidationRule('isColorNameUnique', (value) =>
       this.props.colors.every(
         ({ name }) => name.toLowerCase() !== value.toLowerCase()
       )
     );
-    ValidatorForm.addValidationRule("isColorUnique", value =>
+    ValidatorForm.addValidationRule('isColorUnique', (value) =>
       this.props.colors.every(({ color }) => color !== this.state.currentColor)
     );
   }
@@ -28,16 +28,16 @@ class ColorPickerForm extends Component {
   }
   handleChange(evt) {
     this.setState({
-      [evt.target.name]: evt.target.value
+      [evt.target.name]: evt.target.value,
     });
   }
   handleSubmit() {
     const newColor = {
       color: this.state.currentColor,
-      name: this.state.newColorName
+      name: this.state.newColorName,
     };
     this.props.addNewColor(newColor);
-    this.setState({ newColorName: "" });
+    this.setState({ newColorName: '' });
   }
 
   render() {
@@ -50,33 +50,37 @@ class ColorPickerForm extends Component {
           onChangeComplete={this.updateCurrentColor}
           className={classes.picker}
         />
-        <ValidatorForm onSubmit={this.handleSubmit} ref='form'>
+        <ValidatorForm
+          onSubmit={this.handleSubmit}
+          ref="form"
+          instantValidate={false}
+        >
           <TextValidator
             value={newColorName}
             className={classes.colorNameInput}
-            placeholder='Color Name'
-            name='newColorName'
-            variant='filled'
-            margin='normal'
+            placeholder="Color Name"
+            name="newColorName"
+            variant="filled"
+            margin="normal"
             onChange={this.handleChange}
-            validators={["required", "isColorNameUnique", "isColorUnique"]}
+            validators={['required', 'isColorNameUnique', 'isColorUnique']}
             errorMessages={[
-              "Enter a color name",
-              "Color name must be unique",
-              "Color already used!"
+              'Enter a color name',
+              'Color name must be unique',
+              'Color already used!',
             ]}
           />
           <Button
-            variant='contained'
-            type='submit'
-            color='primary'
+            variant="contained"
+            type="submit"
+            color="primary"
             disabled={paletteIsFull}
             className={classes.addColor}
             style={{
-              backgroundColor: paletteIsFull ? "grey" : currentColor
+              backgroundColor: paletteIsFull ? 'grey' : currentColor,
             }}
           >
-            {paletteIsFull ? "Palette Full" : "Add Color"}
+            {paletteIsFull ? 'Palette Full' : 'Add Color'}
           </Button>
         </ValidatorForm>
       </div>
